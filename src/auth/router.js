705dfd0546dc719a10b2817da0304897e5d42f14
Tanner Seramur <seramurt@gmail.com>
2019-01-19 15:00:54 -0800

    work in progress

---
+++ src/auth/router.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const User = require('./users-model.js');
+const auth = require('./middleware.js');
+const oauth = require('./oauth/google.js');
+const Role = require('./roles-model');
+
+
+//✅  echo '{"username":"tanner", "password":"kona"}' | http post :3000/signup
+authRouter.post('/signup', (req, res, next) => {
+  let user = new User(req.body);
+  user.save()
+    .then( (user) => {
+      User.findOne({_id: user._id})
+        .then(user => {
+          req.token = user.generateToken();
+          req.user = user;
+          res.set('token', req.token);
+          res.cookie('auth', req.token);
+          res.send(req.token);
+        });
+    })
+    .catch(next);
+});
+
+authRouter.post('/newRole', (req,res,next) => {
+  let role = new Role(req.body);
+  role.save()
+  .then(role => {
+    res.status(200).send('here is a new role');
+  })
+  .catch(next);
+});
+
+// ✅  http post :3000/signin -a tanner:kona
+authRouter.post('/signin', auth('capability'), (req, res, next) => {
+  res.cookie('auth', req.token);
+  res.send(req.token);
+});
+
+authRouter.get('/oauth', (req,res,next) => {
+  oauth.authorize(req)
+    .then( token => {
+      res.status(200).send(token);
+    })
+    .catch(next);
+});
+
+authRouter.post('/key', auth, (req,res,next) => {
+  let key = req.user.generateKey();
+  res.status(200).send(key);
+});
+
+module.exports = authRouter;
