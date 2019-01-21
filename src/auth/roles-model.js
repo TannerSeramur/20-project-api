996376a7fe34de337d80fa1bd3f90bb28e6ce995
Tanner Seramur <seramurt@gmail.com>
2019-01-21 07:59:23 -0800

    push

--- src/auth/roles-model.js
+++ src/auth/roles-model.js
@@ -1,10 +1,32 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const SINGLE_USE_TOKENS = !!process.env.SINGLE_USE_TOKENS;
+const TOKEN_EXPIRE = process.env.TOKEN_LIFETIME || '5m';
+const SECRET = process.env.SECRET || 'foobar';
+
 
 const rolesSchema = new mongoose.Schema({
   role: {type: String, required:true},
   capabilities: {type: [String], required:true},
 });
+
+rolesSchema.methods.generateToken = function(type) {
+  
+  let token = {
+    id: this._id,
+    capabilities: this.capabilities,
+    type: this.role || 'user',
+  };
+  
+  let options = {};
+  if ( type !== 'key' && !! TOKEN_EXPIRE ) { 
+    options = { expiresIn: TOKEN_EXPIRE };
+  }
+
+  return jwt.sign(token, SECRET, options);
+}
 
 module.exports = mongoose.model('roles', rolesSchema);
